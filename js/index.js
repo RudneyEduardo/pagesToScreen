91cf2724afbcc144d675433a34868628534c572b
Rudney eduardo souza vieira <50462041+RudneyEduardo@users.noreply.github.com>
2023-11-27 22:40:31 -0300

    Consume json for carousel and select

--- js/index.js
+++ js/index.js
@@ -1,22 +1,54 @@
+
 //main func
 const loadPage = () => {
+    setWorksSelect();
+    let workId = getWorkIdParam()
+    populateDivs(workId)
+}
+
+
+async function getJsonData() {
+    return await fetch('./data.json')
+        .then((res) => {
+            return res.json();
+        })
+}
+
+
+const populateDivs = (workId) => {
+    console.log("WORK ID: ", workId)
+}
+
+const getWorkIdParam = () => {
     const params = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
-      });
-      // Get the value of "page" in eg "https://example.com/?page=scottPilgrim"
-      let value = params.page; //Will receive the page value to show
-      
+    });
+    // Get the value of "workId" in eg "pages/works/index?workId=0 => scott pilgrim"
+    return params.workId; //Will receive the page value to show
+
+}
+
+function selectWork() {
+    let selectValue = document.getElementById('worksSelect').value
+    //send to work page
+    window.location.href = `/pages/works/index.html?workId=${selectValue}`;
+}
+
+async function setWorksSelect() {
+    //Get data from local json
+    let data = await getJsonData();
+    console.log(data)
+    //add dynamically text to select
+    let worksSelect = document.getElementById("worksSelect")
+
+    for (let i = 0; i < data.files.length; i++) {
+        let option = document.createElement("option");
+        option.value = i
+        option.text = data.files[i].title;
+        worksSelect.add(option);
+    }
 }
 
 
-function Func() {
-    fetch('./data.json')
-        .then((res) => {
-        return res.json();
-    })
-    .then((data) => console.log(data));
-}
 
 
-
-Func()
--- js/slideShow.js
+++ js/slideShow.js
@@ -1,5 +1,26 @@
 let slideIndex = 0;
 showSlides();
+setImageCaptions();
+setWorksSelect();
+
+
+async function getJsonData() {
+  return await fetch('./data.json')
+    .then((res) => {
+      return res.json();
+    })
+}
+
+async function setImageCaptions() {
+  //Get data from local json
+  let data = await getJsonData();
+  //add dynamically text to caption
+  let captions = document.getElementsByClassName('text');
+  for (let i = 0; i < captions.length; i++) {
+    let text = document.createTextNode(data.works[i].title)
+    captions[i].appendChild(text)
+  }
+}
 
 function showSlides() {
   let i;
@@ -8,7 +29,28 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+function selectWork() {
+  let selectValue = document.getElementById('worksSelect').value
+  //send to work page
+  window.location.href = `/pages/works/index.html?workId=${selectValue}`;
+}
+
+async function setWorksSelect() {
+  //Get data from local json
+  let data = await getJsonData();
+  //add dynamically text to select
+  let worksSelect = document.getElementById("worksSelect")
+
+  for (let i = 0; i < data.works.length; i++) {
+    let option = document.createElement("option");
+    option.value = i
+    option.text = data.works[i].title;
+    worksSelect.add(option);
+  }
+}
+
